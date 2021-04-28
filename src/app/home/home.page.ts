490fcbf332e903c043a7f1654ad2cf8004751c35
Sophia Martins <79290630+sophiafmartins@users.noreply.github.com>
2021-04-28 00:54:50 -0300

    implementando a busca na home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -18,6 +18,10 @@ interface Receita {
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
+
+  public buscaAtivada: boolean = false;
+  public textoBusca: string;
+  public listaBusca: Receita[] = [];
   public listaReceitas: Receita[] = [
     {
       id: 1,
@@ -144,6 +148,16 @@ export class HomePage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public buscar(){
+    if(this.textoBusca.length != 0 && this.textoBusca.trim().length != 0){
+      this.listaBusca = this.listaReceitas.filter(umaReceita => umaReceita.nome.toLowerCase().includes(this.textoBusca.toLowerCase()));
+      this.buscaAtivada = true;
+    }else{
+      this.listaBusca = [];
+      this.buscaAtivada = false;
+    }
   }
 
 }
