bd9534327c84d5d3b5c74326e8f30e55b69d02e7
Sophia Martins <79290630+sophiafmartins@users.noreply.github.com>
2021-05-15 01:56:15 -0300

    começando algumas alterações para a entrega 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { IonicStorageModule } from '@ionic/storage-angular';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), IonicStorageModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/busca/busca.page.ts
+++ src/app/busca/busca.page.ts
@@ -3,18 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { ModalFiltroComponent } from '../modal-filtro/modal-filtro.component';
 
-type Categorias = 'Entrada' | 'Principal' | 'Sobremesa' | 'Lanche' | null;
-type Tipos = 'Doce' | 'Salgado' | null;
+import { Receita, ReceitasService} from '../services/receitas.service';
 
-interface Receita {
-  id: number,
-  nome: string,
-  tipo: Tipos,
-  categoria: Categorias,
-  porcoes: number,
-  tempo: number,
-  ingredientes: string[]
-}
 
 @Component({
   selector: 'app-busca',
@@ -23,153 +13,18 @@ interface Receita {
 })
 export class BuscaPage implements OnInit {
 
+  public receitas: Receita[] = this.receitaService.receitas;
+
   public buscaAtivada: boolean = false;
   public filtroAtivado: boolean = false;
   public textoBusca: string;
   public filtrosSelecionados: string[] = null;
-  public listaReceitas: Receita[] = [
-    {
-      id: 1,
-      nome: 'Bolo de cenoura',
-      tipo: 'Doce',
-      categoria: 'Sobremesa',
-      porcoes: 8,
-      tempo: 40,
-      ingredientes: ['Óleo', 'Cenoura', 'Ovo', 'Açúcar', 'Farinha de trigo', 'Fermento em pó', 'Manteiga', 'Chocolate em pó', 'Leite']
-    },
-    {
-      id: 2,
-      nome: 'Torta de frango',
-      tipo: 'Salgado',
-      categoria: 'Lanche',
-      porcoes: 15,
-      tempo: 40,
-      ingredientes: ['Frango', 'Caldo de galinha', 'Óleo', 'Alho', 'Cebola', 'Tomate', 'Ervilha', 'Sal', 'Pimenta-do-reino', 'Leite', 'Ovo', 'Farinha de trigo', 'Fermento em pó', 'Queijo']
-    },
-    {
-      id: 3,
-      nome: 'Salada',
-      tipo: 'Salgado',
-      categoria: 'Entrada',
-      porcoes: 6,
-      tempo: 30,
-      ingredientes: ['Alface', 'Cenoura', 'Beterraba', 'Tomate', 'Cebola', 'Sal', 'Açúcar', 'Azeite', 'Vinagre']
-    },
-    {
-      id: 4,
-      nome: 'Beijinho',
-      tipo: 'Doce',
-      categoria: 'Sobremesa',
-      porcoes: 50,
-      tempo: 20,
-      ingredientes: ['Leite condensado', 'Manteiga', 'Coco', 'Açúcar', 'Cravos-da-índia']
-    },
-    {
-      id: 5,
-      nome: 'Torta de limão',
-      tipo: 'Doce',
-      categoria: 'Sobremesa',
-      porcoes: 15,
-      tempo: 40,
-      ingredientes: ['Biscoito de maisena', 'Margarina', 'Leite condensado', 'Creme de leite', 'Limão', 'Ovo', 'Açúcar']
-    },
-    {
-      id: 6,
-      nome: 'Macarrão bolonhesa',
-      tipo: 'Salgado',
-      categoria: 'Principal',
-      porcoes: 6,
-      tempo: 45,
-      ingredientes: ['Cebola', 'Azeite', 'Azeitona', 'Molho de tomate', 'Caldo de carne', 'Cenoura', 'Tomate', 'Macarrão', 'Carne moída']
-    },
-    {
-      id: 7,
-      nome: 'Salada de frutas',
-      tipo: 'Doce',
-      categoria: 'Sobremesa',
-      porcoes: 6,
-      tempo: 10,
-      ingredientes: ['Leite condensado', 'Limão', 'Morango', 'Laranja', 'Kiwi', 'Manga', 'Frutas']
-    },
-    {
-      id: 8,
-      nome: 'Lasanha de frango',
-      tipo: 'Salgado',
-      categoria: 'Principal',
-      porcoes: 4,
-      tempo: 45,
-      ingredientes: ['Frango', 'Queijo', 'Presunto', 'Massa de lasanha', 'Requeijão', 'Caldo de galinha', 'Leite', 'Creme de leite', 'Farinha', 'Manteiga', 'Cebola']
-    },
-    {
-      id: 9,
-      nome: 'Brigadeiro',
-      tipo: 'Doce',
-      categoria: 'Sobremesa',
-      porcoes: 30,
-      tempo: 25,
-      ingredientes: ['Leite condensado', 'Margarina', 'Chocolate em pó', 'Achocolatado', 'Chocolate granulado']
-    },
-    {
-      id: 10,
-      nome: 'Arroz com brócolis',
-      tipo: 'Salgado',
-      categoria: 'Principal',
-      porcoes: 6,
-      tempo: 30,
-      ingredientes: ['Arroz', 'Alho', 'Azeite', 'Brócolis', 'Bicarbonato de sódio', 'Água', 'Sal']
-    },
-    {
-      id: 11,
-      nome: 'Bolo de laranja',
-      tipo: 'Doce',
-      categoria: 'Sobremesa',
-      porcoes: 12,
-      tempo: 40,
-      ingredientes: ['Ovo', 'Açúcar', 'Óleo', 'Laranja', 'Farinha de trigo', 'Fermento']
-    },
-    {
-      id: 12,
-      nome: 'Curau de milho',
-      tipo: 'Doce',
-      categoria: 'Sobremesa',
-      porcoes: 14,
-      tempo: 15,
-      ingredientes: ['Milho', 'Açúcar', 'Leite', 'Amido de milho']
-    },
-    {
-      id: 13,
-      nome: 'Couve-flor gratinada',
-      tipo: 'Salgado',
-      categoria: 'Entrada',
-      porcoes: 8,
-      tempo: 30,
-      ingredientes: ['Couve-flor', 'Requeijão', 'Ovo', 'Queijo', 'Mussarela', 'Cheiro-verde', 'Tomate', 'Margarina']
-    },
-    {
-      id: 14,
-      nome: 'Batata recheada',
-      tipo: 'Salgado',
-      categoria: 'Entrada',
-      porcoes: 2,
-      tempo: 30,
-      ingredientes: ['Batata', 'Sal', 'Pimenta-do-reino', 'Ovo', 'Queijo', 'Bacon', 'Creme de leite', 'Cebolinha']
-    },
-    {
-      id: 15,
-      nome: 'Esfiha de carne',
-      tipo: 'Salgado',
-      categoria: 'Lanche',
-      porcoes: 30,
-      tempo: 90,
-      ingredientes: ['Fermento', 'Açúcar', 'Sal', 'Leite', 'Água', 'Ovo', 'Farinha de trigo', 'Óleo', 'Carne moída', 'Tomate', 'Cebola', 'Alho', 'Azeitona', 'Noz moscada', 'Salsinha', 'Cebolinha', 'Molho de pimenta', 'Gema', 'Gergelim']
-    },
-  ]
-
+  public listaReceitas: Receita[] = this.receitas;
   public listaFiltrada: Receita[] = this.listaReceitas;
   public listaBusca: Receita[] = [];
 
 
-  constructor(public modalCtrl: ModalController) { }
+  constructor(public modalCtrl: ModalController, private receitaService: ReceitasService) { }
 
   ngOnInit() {
   }
@@ -198,8 +53,8 @@ export class BuscaPage implements OnInit {
       this.filtroAtivado = true;
       for(let f of this.filtrosSelecionados){
         for(let r of this.listaReceitas){
-          for(let i=0; i < r.ingredientes.length; i++){
-            if(r.tipo === f['nome'] || r.categoria === f['nome'] || r.ingredientes[i] === f['nome']){
+          for(let i=0; i < r.ingrdFiltro.length; i++){
+            if(r.tipo === f['nome'] || r.categoria === f['nome'] || r.ingrdFiltro[i] === f['nome']){
               if(!this.listaFiltrada.find(umaReceita => umaReceita === r)){
                 this.listaFiltrada.push(r);
               }
